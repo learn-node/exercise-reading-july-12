3c1244fea8de0b024d03ca6183f8e471ea1c1ca3
july-12 <seventanyb@gmail.com>
2022-07-27 13:10:54 +0800

    fix: test

--- hello2.test.js
+++ hello2.test.js
@@ -2,7 +2,7 @@
 const hello = require('./hello');
 
 test('outputs the correct string', () => {
-  expect(hello('faith')).toBe("Hello faith2!");
+  expect(hello('faith')).toBe("Hello faith!");
 });
 
 test('outputs the not string', () => {
